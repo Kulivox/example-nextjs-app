ca2453e9f17971edd23f573cf473a0c613d5401a
Michal Klinka <mich.klinka@gmail.com>
2024-03-12 18:00:31 +0100

    Added scrolling to the todos

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import {Todos} from "@/app/todos";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center space-y-3 justify-start p-24">
-      <Todos />
-      <NewTodo />
+      <div className="w-full h-[calc(100vh-20rem)] overflow-auto">
+          <Todos />
+      </div>
+      <div className="w-full sticky bottom-20 ">
+          <NewTodo />
+      </div>
     </main>
   );
 }
